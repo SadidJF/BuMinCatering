dd064aac4a7cad12fb63ba9cadae29b9ffaacced
SadidJF <sadidjundif.project@gmail.com>
2022-09-29 11:55:53 +0700

    new update

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
-getElementById('id')
+const toggleButton = document.getElementById('toggle-button')
+const naviList = document.getElementById('navi-list')
+
+toggleButton.addEventListener('click', () => {
+    naviList.classList.toggle('active');
+})
